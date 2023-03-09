d31d68fa9421eeb904b832189d956ee3bbb730c5
Lucas Phillips <lucas.phillips@solirius.com>
2023-03-09 15:43:24 +0000

    Created update route

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,12 +9,35 @@ class EntriesController < ApplicationController
 
   def create
     @entry = Entry.new(entry_params)
+    @is_new = true
     if @entry.save
       redirect_to root_url
     else
-      render :new
+      render :edit
     end
   end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
+    @is_new = false
+    render :edit
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+    if @entry.update(params.require(:entry).permit(:title, :url))
+      flash[:success] = "To-do item successfully updated!"
+      redirect_to entry_url(@entry)
+    else
+      flash.now[:error] = "To-do item update failed"
+      render :edit
+    end
+  end
+
   def destroy
     Entry.find(params[:id]).destroy
     redirect_to root_url
